bc78e8421ed5200318978c44424dab6ec69d7695
reszeinz <120090328+reszeinz@users.noreply.github.com>
2022-12-08 22:24:23 +0800

    New files

---
+++ js/audio_control.js
@@ -0,0 +1,14 @@
+var myAudio = document.getElementById("suiseinamba1");
+const delay = ms => new Promise(res => setTimeout(res, ms));
+myAudio.volume = 0.4;
+
+window.onload = async function () {
+    await delay(2500);
+    if (myAudio.paused) {
+        myAudio.play();
+    }
+}
+
+function togglePlay() {
+    return myAudio.paused ? myAudio.play() : myAudio.pause();
+};
